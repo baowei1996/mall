8f70fb2e4c2fcb736c36e728eedb2ae75f86b78a
bobking <m17864154913@163.com>
2020-08-10 11:18:23 +0800

    整合ElasticSearch实现搜索功能

--- src/main/java/bobking/icu/mall/common/api/CommonPage.java
+++ src/main/java/bobking/icu/mall/common/api/CommonPage.java
@@ -3,6 +3,7 @@ package bobking.icu.mall.common.api;
 import com.github.pagehelper.PageInfo;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -15,6 +16,9 @@ public class CommonPage<T> {
     private Long total;
     private List<T> list;
 
+    /**
+     * 将PageHelper分页后的list转为分页信息
+     */
     public static <T> CommonPage<T> restPage(List<T> list) {
         CommonPage<T> result = new CommonPage<>();
         PageInfo<T> pageInfo = new PageInfo<>(list);
@@ -23,6 +27,19 @@ public class CommonPage<T> {
         result.setPageSize(pageInfo.getPageSize());
         result.setTotal(pageInfo.getTotal());
         result.setList(pageInfo.getList());
+        return result;
+    }
+
+    /**
+     * 将SpringData分页后的list转为分页信息
+     */
+    public static <T> CommonPage<T> restPage(Page<T> pageInfo) {
+        CommonPage<T> result = new CommonPage<T>();
+        result.setTotalPage(pageInfo.getTotalPages());
+        result.setPageNum(pageInfo.getNumber());
+        result.setPageSize(pageInfo.getSize());
+        result.setTotal(pageInfo.getTotalElements());
+        result.setList(pageInfo.getContent());
         return result;
     }
 }
---
+++ src/main/java/bobking/icu/mall/service/EsProductService.java
@@ -0,0 +1,34 @@
+package bobking.icu.mall.service;
+
+import bobking.icu.mall.nosql.elasticsearch.document.EsProduct;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+//这里的增删改查只是ElasticSearch中的增删改查，不会影响数据库
+public interface EsProductService {
+    /**
+     * 从数据库中导入所有商品到es
+     */
+    int importAll();
+
+    /**
+     * 根据id删除商品
+     */
+    void delete(Long id);
+
+    /**
+     * 根据id创建商品
+     */
+    EsProduct create(Long id);
+
+    /**
+     * 批量删除商品
+     */
+    void delete(List<Long> ids);
+
+    /**
+     * 根据关键字搜索名称或者副标题
+     */
+    Page<EsProduct> search(String keyword, Integer pageNum, Integer pageSize);
+}
